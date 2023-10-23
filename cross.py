60d6540e37f6113a4d2fd6b4ab7a65393bf4fc2f
blackantywj <ywjysys520@outlook.com>
2023-10-23 14:45:02 +0800

    first commit

---
+++ cross.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+# 定义softmax函数
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x))
+
+# 利用numpy计算
+def cross_entropy_np(x, y):
+    x_softmax = [softmax(x[i]) for i in range(len(x))]
+    x_log = [np.log(x_softmax[i][y[i]]) for i in range(len(y))]
+    loss = - np.sum(x_log) / len(y)
+    return loss
+
+# 测试逻辑
+x = [[1.9269, 1.4873, 0.9007, -2.1055]]
+y = [[2]]
+v1 = cross_entropy_np(x, y)
+print(f"v1: {v1}")
+
+x = torch.unsqueeze(torch.Tensor(x), dim=0)
+x = x.transpose(1, 2)  # CrossEntropy输入期望: Class放在第2维，Batch放在第1维
+
+y = torch.Tensor(y)
+y = y.to(torch.long)  # label的类型为long
+
+v2 = F.cross_entropy(x, y, reduction="none")
+print(f"v2: {v2}")
